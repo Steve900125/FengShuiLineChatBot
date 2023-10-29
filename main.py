f05601edbb78943ba3771e3c55f6de80b838cff3
Steve900125 <900125kevin@gmail.com>
2023-10-29 16:03:38 +0800

    add chatgpt response

--- main.py
+++ main.py
@@ -62,7 +62,15 @@ configuration = Configuration(access_token=line_api)
 handler = WebhookHandler(secrect_api)
 # 'MY_CHANNEL_SECRET'
 
-# App route
+# ChatGPT call import
+#============================================================================#
+from chatgpt_response import call_chatgpt
+
+
+count = 0
+# 測試數量限制 5 
+
+# callback : Hang on the web address on Line Webhook
 #============================================================================#
 
 @app.route("/callback", methods=['POST'])
@@ -84,59 +92,64 @@ def callback():
     return 'OK'
 
 
+# When the user sends the messages, this code will be activated.
+#============================================================================#
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
-
+# 取得使用者名稱
+#============================================================================#
         url = 'https://api.line.me/v2/bot/profile/' + event.source.user_id
-        # 用 
+        # 把傳入的使用者 id 當作參數用 api 去取得用戶資料名稱
         headers = {
             'Authorization': 'Bearer ' + line_api
             }
+        # channel access token 授權
         response = requests.get(url, headers = headers)
 
         user_name = 'Hello !'
         if response.status_code == 200:
             user_profile = response.json()
-            user_name = user_name + user_profile['displayName'] + " 以下是您的訊息紀錄： "
-            
+            user_name = user_name + user_profile['displayName'] + " 以下是您的訊息紀錄： "   
+
+# Database load data
+# Maybe lode user data by ID then check meassages
+#============================================================================#
+     
+
+# Call chatGPT to answer the question
+# Maybe can select which models suit the question type
+#============================================================================#
         
+        
+
+        if event.message.type == 'text' and count < 5:
+            ans = call_chatgpt(user_question = event.message.text , user_data = [] ,user_id = '')
+            count = count + 1
+        elif event.message.type != 'text':
+            ans = '嗨目前只能接受文字回覆喔 你的回覆種類是' + event.message.type
+        else:
+            ans = '今日使用上限已額滿'
+
+        
+
+# Return response to user
+#============================================================================#
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token = event.reply_token,
-                messages=[TextMessage(text = user_name + event.message.text)]
+                messages=[TextMessage(text = user_name + ans)]
+                # 回傳資料的地方
             )
         )
 
-        print(type(event.message.text))
-        print(event.message.text)
-        print(type(event))
-        print(f'Event: {event}')
-        print(type(event.message))
-        print(event.message)
-        print('user id' + event.source.user_id)
 
-        
-        
-@app.route("/hi")
-def hi():
-    return 'hi'
+@app.route("/testweb")
+def testweb():
+    gpt_ans = call_chatgpt(user_question = '請問狗狗跑多快？' , user_data = [] ,user_id = '')
+    return gpt_ans
 
-@app.route("/")
-def home():
-    return 'You are in home'
-
-@app.route("/apikey")
-def apikey_check():
-    return 'line :' + line_api + '\n secrect_api:' + secrect_api
-
-@app.route("/test")
-def test():
-    result = ''
-    # First SQL
-    print('test')
-    return 'this is test'
 
     
 
